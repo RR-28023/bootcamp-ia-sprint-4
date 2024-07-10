aa0e7ccb7548cc65c7fbc534ddef0ae0fb69c583
Vladimir Maksimov <vladimirmaksimov@Vladimirs-MacBook-Pro.local>
2024-07-10 19:58:12 +0200

    Ran experiments with different models, kept the one which performs well. Modified functions that get synopsis and query and return cleaned versions

--- retrieval/indexing_pipeline_utils.py
+++ retrieval/indexing_pipeline_utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+
 from langchain_core.documents import Document
 
 from data_utils import Movie
@@ -27,4 +29,29 @@ def create_docs_to_embedd(movies: list[Movie], config: config.RetrievalExpsConfi
 def get_synopsys_txt(movie: Movie) -> str:
     return movie.synopsis
 
-# def ...
+
+def get_synopsys_and_genre_txt(movie: Movie) -> str:
+    return f'synopsis: {movie.synopsis}, genre: {movie.genre_tags}'
+
+
+def get_clean_synopsis_and_genre_txt(movie: Movie) -> str:
+    file = open(
+        "/Users/vladimirmaksimov/Dev/Todo/bootcamp-ia-sprint-4/retrieval/data/spanish.txt",
+        encoding="utf8"
+    )
+    stop_words = []
+    for stop_word in file:
+        stop_words.append(str(stop_word.strip()))
+    synopsis = movie.synopsis
+    synopsis = synopsis.replace("(FILMAFFINITY)", "").strip()
+    # Delete special characters and punctuation.
+    synopsis = re.sub(r'[^A-Za-z\s, á, é, ú, ü, í, ó, ñ, 0-9]', '', synopsis)
+    synopsis = re.sub(r'[,]+' , '' , synopsis)
+    # Replace upper letters with lower ones.
+    synopsis = synopsis.lower()
+    # Split the query into the list of words.
+    synopsis_list = list(synopsis.split())
+    # Join the words into a phrase without stop words.
+    synopsis = ' '.join([word for word in synopsis_list if word not in stop_words])
+    return f'synopsis: {synopsis}, genre: {movie.genre_tags}'
+
--- retrieval/retrieval_pipeline_utils.py
+++ retrieval/retrieval_pipeline_utils.py
@@ -1,3 +1,24 @@
+import re
+
+
 def clean_query_txt(query: str) -> str:
     query = query.replace("El usuario busca ", "").strip()
+    return query
+
+
+def new_clean_query_txt(query: str) -> str:
+    file = open("/Users/vladimirmaksimov/Dev/Todo/bootcamp-ia-sprint-4/retrieval/data/spanish.txt", encoding="utf8")
+    stop_words = []
+    for stop_word in file:
+        stop_words.append(str(stop_word.strip()))
+    query = query.replace("El usuario busca ", "").strip()
+    # Delete special characters, numbers and punctuation.
+    query = re.sub(r'[^A-Za-z\s, á, é, ú, ü, í, ó, ñ]', '', query)
+    query = re.sub(r'[,]+' , '' , query)
+    # Replace upper letters with lower ones.
+    query = query.lower()
+    # Split the query into the list of words.
+    query_list = list(query.split())
+    # Join the words into a phrase without stop words.
+    query = ' '.join([word for word in query_list if word not in stop_words])
     return query
