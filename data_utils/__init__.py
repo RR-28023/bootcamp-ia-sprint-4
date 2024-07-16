3cd7ebfd89852351c5dde7c7f7bec097d6d1f5ff
mlpalacios8 <marcelpalacios8@gmail.com>
2024-07-16 07:59:16 +0200

    Deliverable RAG

--- data_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .db_utils import get_movies_data
-from .schemas import Movie
--- retrieval/retrieval_pipeline_utils.py
+++
@@ -1,3 +0,0 @@
-def clean_query_txt(query: str) -> str:
-    query = query.replace("El usuario busca ", "").strip()
-    return query
