7cee4dbe06c89b3e904d3c58eb595e53469f61b1
Yeray <yera_hb@hotmail.es>
2024-07-18 12:01:53 +0100

    Mejorando limpieza de query

--- retrieval/retrieval_pipeline_utils.py
+++ retrieval/retrieval_pipeline_utils.py
@@ -1,3 +1,12 @@
+import re
+from nltk.corpus import stopwords
+
 def clean_query_txt(query: str) -> str:
     query = query.replace("El usuario busca ", "").strip()
+    # Eliminar múltiples espacios intermedios
+    query = re.sub(r'\s+', ' ', query)
+    # Convertir a minúsculas da peores resultados
+    # Eliminar caracteres especiales, mantener solo letras y números
+    query = re.sub(r'[^\w\s]', '', query)
+    # Eliminar stopwords da peores resultados
     return query
