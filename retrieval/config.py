4aa94fb5467a7be936e28b4c734a6afb3cab20ea
Yeray <yera_hb@hotmail.es>
2024-07-18 12:07:15 +0100

    New models

--- retrieval/config.py
+++ retrieval/config.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Callable
 
 from data_utils import Movie
-from retrieval.indexing_pipeline_utils import get_synopsys_txt
+from retrieval.indexing_pipeline_utils import get_synopsys_and_genres_txt, get_synopsis_genre_and_director, get_synopsis_genre_country_and_director, get_synopsis_genre_and_country
 from retrieval.retrieval_pipeline_utils import clean_query_txt
 
 
@@ -19,11 +19,11 @@ class RetrievalExpsConfig:
     def __init__(self):
 
         # Función a emplear para generar el texto a indexar con embeddings; Debe tomar como input un objeto `Movie` y devolver un string
-        self._text_to_embed_fn: Callable = get_synopsys_txt
+        self._text_to_embed_fn: Callable = get_synopsis_genre_and_director
 
         # Parámetros para la generación de embeddings
 
-        self.model_name: str = "all-MiniLM-L6-v2"
+        self.model_name: str = "paraphrase-multilingual-mpnet-base-v2"
         self.normalize_embeddings: bool = False  # Normalizar los embeddings a longitud 1 antes de indexarlos
 
         self._query_prepro_fn: Callable = clean_query_txt
