c9e10f529129a52961bc6d4baeb8263b9643e6cf
alex-candela <alex.candela@outlook.com>
2024-11-18 14:43:34 +0100

    updated - clean query and text embedding

--- retrieval/retrieval_pipeline_utils.py
+++ retrieval/retrieval_pipeline_utils.py
@@ -1,3 +1,9 @@
-def clean_query_txt(query: str) -> str:
-    query = query.replace("El usuario busca ", "").strip()
-    return query
+import re
+from nltk.corpus import stopwords
+
+def clean_query_txt(texto: str) -> str:
+    texto = texto.replace("El usuario busca ", "").strip()
+    texto = texto.replace("película", "").strip()
+    # Convertir la primera palabra a minúsculas
+    texto = texto.lower()
+    return texto
